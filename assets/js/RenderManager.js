4793f53eabb0bc270db20c43b30c85bc49386728
Taka005 <01taka005@gmail.com>
2024-04-21 21:55:17 +0900

    change:ミス調整

--- assets/js/RenderManager.js
+++ assets/js/RenderManager.js
@@ -22,8 +22,6 @@ export default class RenderManager{
       .forEach(component=>{
         component.draw(this.ctx);
       });
-
-    console.log(this.elements);
   }
 
   /**
--- assets/js/pages/Base.js
+++ assets/js/pages/Base.js
@@ -3,6 +3,7 @@ export default class Base{
     this.type = "page";
     this.rank = 0;
     this.isDisplay = false;
+    this.isChange = true;
   }
 
   /**
@@ -11,6 +12,16 @@ export default class Base{
    */
   setDisplay(bool){
     this.isDisplay = bool;
+
+    return this;
+  }
+
+  /**
+   * 変更可能状態を設定
+   * @param {Boolean} bool 変更するかどうか
+   */
+  setChange(bool){
+    this.isChange = bool;
 
     return this;
   }
